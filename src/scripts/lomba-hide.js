0630b472ca11d59d64e6bee8ea35897260884823
nvll-zen <nxll.zen@gmail.com>
2024-08-22 14:08:42 +0700

    Optimizing lomba js

---
+++ src/scripts/lomba-hide.js
@@ -0,0 +1,16 @@
+let elements = [
+    document.getElementById("lomba1"),
+    document.getElementById("lomba2"),
+    document.getElementById("lomba3"),
+    document.getElementById("lomba4"),
+    document.getElementById("lomba5"),
+    document.getElementById("lomba6")
+]
+
+co
+
+
+
+
+
+
